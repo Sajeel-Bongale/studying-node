f55b585e0085a4b387a09480b7b1db4dc037dd9c
Sajeel-Bongale <sajeelbongale@gmail.com>
2021-10-04 10:24:03 +0530

    Sets up store-api project with introduction

---
+++ john-smilga-tutorial/mongodb/002.store-api/000.introduction.js
@@ -0,0 +1,15 @@
+/*
+    Youtube Link: https://www.youtube.com/watch?v=rltfdjcXjmk&list=PLnHJACx3NwAdl4yeJF6LzjDiLyW1yF9Ds&index=2&t=11233s
+    Github Link: https://github.com/Sajeel-Bongale/store-api
+
+    This documentation will refer two separate places.
+    1. The videos walking through the creation of the project and
+    2. The actual github project
+
+    In the first commit for the store-api GitHub project, we will set up the project and add content to run the
+    app with the addition of some basic middleware.
+
+    // Personal Note: A postman collection has also been setup with the same name of the project.
+
+    All the documentation of MongoDB will be done in the subsequent JS files.
+ */
