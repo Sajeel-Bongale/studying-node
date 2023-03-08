b1fdfd7952dfca2453792d5e0f617f6da74d12b3
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-03-08 12:34:55 -0500

    Adding a period to the file for testing

--- acade-mind/005.working-with-expresss/002.install-and-run-express.js
+++ acade-mind/005.working-with-expresss/002.install-and-run-express.js
@@ -4,6 +4,6 @@
     To install express add it using npm install express in your project. To use it require it.
 
     The express module is a reference to a function. You call this function and store the result in a
-    variable called app. We will use this variable later.
+    variable called app. We will use this variable later..
 
  */
