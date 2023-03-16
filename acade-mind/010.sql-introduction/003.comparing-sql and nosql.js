75eb765f7af6a41b2eb7c60f15a70ee30b38411c
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-03-16 12:53:55 -0400

    003.comparing-sql and nosql.js

---
+++ acade-mind/010.sql-introduction/003.comparing-sql and nosql.js
@@ -0,0 +1,17 @@
+/*
+
+    Link: https://www.udemy.com/course/nodejs-the-complete-guide/learn/lecture/11738946#questions
+
+    Horizontal and vertical are two approaches we can use to scale our databases.
+
+    In horizontal servers, we add more servers and we can do this at infinity as we can buy new servers on cloud or on
+    our data centers and connect them to our database and split our data across these servers.
+
+    While in vertical scaling we make our existing server stronger by adding CPU or memory.
+
+    Horizontal scaling is difficult or impossible as it involves creating many servers. If we have hundreds or
+    thousands of users than reading and writing queries can become complex.
+    Whereas for noSQL scaling is easy and sometimes the packages also helps to create databases for us. It is great for
+    performance when mass requests are to be read and written.
+
+ */
