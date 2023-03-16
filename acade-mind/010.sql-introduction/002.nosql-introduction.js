8ec14bf54180db117858cad1e4c398a2093722da
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-03-15 20:50:00 -0400

    002.nosql-introduction.js

---
+++ acade-mind/010.sql-introduction/002.nosql-introduction.js
@@ -0,0 +1,24 @@
+/*
+
+    Link: https://www.udemy.com/course/nodejs-the-complete-guide/learn/lecture/11738944#questions
+
+    In SQL we had tables but in noSQL we have collections. In collections we find documents. These documents are very
+    close to what we describe in javascript as data -> example 1
+
+    noSQL does not have a strict schema. For example we can have name and age in one collection but just name in
+    another collection. We can store multiple documents with different structures in the same collection.
+
+    In noSQL we do not have real world data but we go for duplicate data. As data duplication is possible we
+    do not have to relate one table to another as most details from one table are also duplicated in another.
+
+    The core features of noSQL database are:
+        1. It does not have strong data schema. So, no structure is required.
+        2. Usually there are no or few connections in noSQL databases.
+
+    example 1
+    {
+        name: "Max",
+        age: 29
+    }
+
+ */
