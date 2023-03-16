64195f76a223181e949264a7e05abefffc08bf08
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-03-16 12:55:38 -0400

    004.setting-up-mysql.js

---
+++ acade-mind/010.sql-introduction/004.setting-up-mysql.js
@@ -0,0 +1,14 @@
+/*
+
+    Link: https://www.udemy.com/course/nodejs-the-complete-guide/learn/lecture/11738948#questions
+
+    To download mysql we need to visit the mysql.com -> Downloads -> Mysql community Downloads -> MySQL Installer for
+    Windows or MySQL Community Server -> Microsoft Windows -> Download the latest version.
+
+    Execute the downloaded file -> Download both work bench and mysql -> Let is install and after few clicks
+    we will prompted to create a password and then it is installed. For most of the selection we had kept the default
+    settings.
+
+    Once done we can open workbench and by clicking on the instance we enter the password and open the instance.
+
+ */
