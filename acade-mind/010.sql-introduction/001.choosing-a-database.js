312d356a995a56a4f48587cd66cb43abbd39d930
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-03-15 20:47:14 -0400

    001.choosing-a-database.js

---
+++ acade-mind/010.sql-introduction/001.choosing-a-database.js
@@ -0,0 +1,19 @@
+/*
+    Link:https://www.udemy.com/course/nodejs-the-complete-guide/learn/lecture/11738942#questions
+
+    Database helps to store and retrieve data easily. It is quicker than accessing the data from the file as the data
+    begins to grow overtime. Database also doesnt require us to read the whole file for finding a particular value.
+
+    SQL is a table which consist of fields which have rows and columns. SQL based database allows us to relate
+    different tables. SQL databases has strong schema in which we define if the field is a number, string, boolean etc
+    and also how we structure the table.
+
+    Thus, core things about SQL databases are:
+        1. The structure of the data looks like (eg: in a table format)
+        2. The data type of each field
+        3. Relating different tables with some important relation like one to many, many to many or one to
+        one. So, the tables can be connected.
+
+    SQL stands for structured query languages. Queries are command we use to interact with the database.
+
+ */
