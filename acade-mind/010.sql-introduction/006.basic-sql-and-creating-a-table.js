285164360569708d7f84cfd89c1f707b26a97b3b
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-03-18 19:50:50 -0400

    006.basic-sql-and-creating-a-table.js

---
+++ acade-mind/010.sql-introduction/006.basic-sql-and-creating-a-table.js
@@ -0,0 +1,13 @@
+/*
+
+    Link: https://www.udemy.com/course/nodejs-the-complete-guide/learn/lecture/11738960#questions
+
+    For creating database on the server we need to open our project on the workBench and add a table in the tables
+    session on the left side bar.
+
+    We then create a table with the column names and the data type we expect in each field.
+
+    Once the table is created we click on the Apply button and our changes are saved. We can then view the table from
+    the sidebar.
+
+ */
