accae77c546f2f8cfec9621e586d5391c6b51c56
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-04-26 22:05:26 -0400

    011.using-magic-associations-methods.js

---
+++ acade-mind/011.understanding-sequelize/011.using-magic-associations-methods.js
@@ -0,0 +1,16 @@
+/*
+
+    Link: https://www.udemy.com/course/nodejs-the-complete-guide/learn/lecture/11739036#questions/5458366
+
+    createProduct method is available when we define a relationship between 2 things. Sequelize helps us to make
+    associations and also provides us a way in which our models know each other.
+
+    example 1
+     req.user.createProduct({
+        title: title,
+        imageUrl: imageUrl,
+        price: price,
+        description: description,
+    })
+
+ */
