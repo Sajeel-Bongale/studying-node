1fcbc384ab6a817edec8303121ca476bce66b48a
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-05-01 19:53:16 -0400

    003.using-modules-1-core-modules.js

---
+++ jonas-schmedtmann/001.introduction-to-node-js-and-npm/003.using-modules-1-core-modules.js
@@ -0,0 +1,14 @@
+/*
+
+    Using Modules 1: Core modules
+
+    To run a particular js file in node we use the following command -> example 1
+
+    Node is build up of different modules in which functionalities are stored in modules like for reading files it is
+    stored in the module fs. To use these modules we first require them and then  store them in a constant and later
+    use that in a constant.
+
+    example 1
+    node FileName.js
+
+ */
