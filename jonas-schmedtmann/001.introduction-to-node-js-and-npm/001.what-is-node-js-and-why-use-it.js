aafdf211f29143ad95de210957c9d73b45a9c9e1
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-05-01 19:51:39 -0400

    001.what-is-node-js-and-why-use-it.js

---
+++ jonas-schmedtmann/001.introduction-to-node-js-and-npm/001.what-is-node-js-and-why-use-it.js
@@ -0,0 +1,23 @@
+/*
+
+    What is NodeJs and why use it?
+
+    NodeJS is a JavaScript runtime built on Google's open-source V8 JavaScript engine. Browser natively understands
+    HTML, CSS and Javascript. So, javascript frameworks like Angular, React can be executed in the browser. So, in
+    this case, the browser is the JavaScript Runtime.
+
+    NodeJs is another Javascript runtime, container or an environment in which we can run javascript programs and
+    execute them but outside of a browser. V8 engine actually parses and run the code in NodeJS and not in the browser.
+
+    NodeJS can be run as a server in which we can access the file system and better networking capabilities.
+
+    Node applications are so fast and so scalable because NodeJS is single threaded based on an event driven,
+    non-blocking I/O model which makes NodeJS very lightweight and efficient.
+
+    Node is a good option for creating realtime chat applications, server-side web application or for data streaming
+    like youtube.
+
+    Node should not be used when creating some super heavy server-side processing like having image manipulations,
+    video conversion, file compression.
+
+ */
