e246b0723e6e3ebe42ae41bf1214b74738565fbf
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-05-01 19:52:47 -0400

    002.running-javascript-outside-the-server.js

---
+++ jonas-schmedtmann/001.introduction-to-node-js-and-npm/002.running-javascript-outside-the-server.js
@@ -0,0 +1,28 @@
+/*
+
+    Running Javascript outside the server
+
+    To write node in the terminal we use the command -> example 1
+
+    We can also write any javascript code in node like mathematical calculations, declaring a constant or use javascript
+    string or arrays methods.
+    To exit from the node CLI we can use the following ways:
+        1. type the command -> .exit
+        2. CTL + D
+
+   To clear the terminal we use the command -> example 2
+
+   To see the methods and classes we can use in Node we use the following command -> example 3
+
+   _ represents the previous results.
+
+    example 1
+    node fileName
+
+    example 2
+    CTL + k
+
+    example 3
+    Click tab
+
+ */
