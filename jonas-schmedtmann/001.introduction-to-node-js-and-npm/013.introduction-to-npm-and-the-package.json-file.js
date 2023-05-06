81443b9911819c72f1ddb0acb8d863cc56414ea4
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-05-05 22:32:53 -0400

    013.introduction-to-npm-and-the-package.json-file.js

---
+++ jonas-schmedtmann/001.introduction-to-node-js-and-npm/013.introduction-to-npm-and-the-package.json-file.js
@@ -0,0 +1,14 @@
+/*
+
+    Introduction to npm and the package.json file
+
+    The node package manager is a command line interface app that automatically comes included with Node JS in which we
+    install and manage Open Source packages. The packages come from the package repository that is also npm.
+
+    Npm is actually both the command line interface app that we use to manage the packages and also the repository
+    itself. It's the largest software registry in the world.
+
+    npmjs.com is where all these packages are hosted. The first thing we do is use the command npm init to add the
+    package.json file in our project.
+
+ */
