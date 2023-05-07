3fb4680bb78181f0bef08b627fb1adbcbfac0a56
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-05-06 21:24:04 -0400

    015.using-third-party-modules.js

---
+++ jonas-schmedtmann/001.introduction-to-node-js-and-npm/015.using-third-party-modules.js
@@ -0,0 +1,9 @@
+/*
+
+    Using Third Party Modules
+
+    Usually we require the core modules first, then the third-party modules, and then our own coming from our local
+    file system. Once we require a module NodeJS automatically knows that it needs to go into the node modules folder
+    in the specified dependency and get the file.
+
+ */
