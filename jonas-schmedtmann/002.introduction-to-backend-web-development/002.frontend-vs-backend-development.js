2069fd50d6fcbf471eb8e6e78d36f36518fb3d78
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-05-08 20:32:21 -0400

    002.frontend-vs-backend-development.js

---
+++ jonas-schmedtmann/002.introduction-to-backend-web-development/002.frontend-vs-backend-development.js
@@ -0,0 +1,21 @@
+/*
+
+    Frontend Vs Backend Development
+
+    Front-end development is about everything that happens in the web browser. It's about designing and building
+    the final website that's going to be visible to the user, and therefore, the name front-end. The front-end developer
+    uses the basic technologies: HTML, CSS, and JavaScript, which together form the front-end technology stack.
+    Now, many modern front-end developers add more stuff to the front-end stack like React or Angular or Redux or
+    GraphQL.
+
+    Backend is about everything that happens on the web server, so everything that is invisible to the final user and
+    therefore the name back-end. A basic server is really just a computer that is connected to the internet
+    which, first, stores a website's files like HTML, CSS, and images, and second, runs an HTTP server that is capable
+    of understanding URLs, requests, and also delivering responses. HTTP server software is what actually communicates
+    with the browser using requests and responses. Therefore, it is like the bridge between the front-end and the
+    back-end of a website or web application.
+
+    A static server can serve static files to the client via HTTP. But we want to develop an application which talks to
+    the databases then we create a dynamic web application.
+
+ */
