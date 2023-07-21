a1cfc2b8e2ef63396b627f33b21da21ff0d4b5cb
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-07-21 19:28:06 -0400

    002.what-is-postman.js

---
+++ jonas-schmedtmann/004.express-lets-start-building-natours-api/002.what-is-postman.js
@@ -0,0 +1,14 @@
+/*
+
+    What is Postman
+
+    Postman is a beautiful tool that allows us to do API testing. It's a little bit like a browser. We can do all kinds
+    of requests and then receive the response simply as text then work with that response. So as they say Postman
+    simplifies API development.
+
+    To install postman, we use the url getpostman.com and download postman.
+
+    We can save requests and create collections and folders to organise the requests. We can run automatic tests.
+    We can set up different development environments or production environments.
+
+ */
